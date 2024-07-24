b3ce7a1a53587c92e9596a22f3e3b4d7a2ab883d
nhopkinson <39384361+nhopkinson@users.noreply.github.com>
2024-07-23 21:41:06 -0400

    wip: create client to handle stock api request

---
+++ millionaire_maker/stock_api/stock_api/client.py
@@ -0,0 +1,40 @@
+from polygon.stocks import StocksClient
+import os
+
+
+class StockClient():
+    def __init__(self, client):
+        client = self.client
+
+    ticker = "AAPL"
+
+    # List Aggregates (Bars)
+    client = StocksClient(api_key=os.getenv("API_KEY"))
+    aggs = []
+    for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_="2023-01-01", to="2023-06-13",
+                              limit=50000):
+        aggs.append(a)
+
+    print(aggs)
+
+    # Get Last Trade
+    trade = client.get_last_trade(ticker=ticker)
+    print(trade)
+
+    # List Trades
+    trades = client.list_trades(ticker=ticker, timestamp="2022-01-04")
+    for trade in trades:
+        print(trade)
+
+    # Get Last Quote
+    quote = client.get_last_quote(ticker=ticker)
+    print(quote)
+
+    # List Quotes
+    quotes = client.list_quotes(ticker=ticker, timestamp="2022-01-04")
+    for quote in quotes:
+        print(quote)
+
+
+stock_client = StockClient
+stock_client.quote
